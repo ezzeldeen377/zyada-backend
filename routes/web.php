d348dfc3011c4a1eada3bdb8edbcfd7b0c617bf0
Ezzeldine Fathy <ezzeldeentv20@gmail.com>
2026-09-01 11:18:22 +0300

    refactor: update home route to point to HomeController@index instead of redirecting to admin login

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,7 @@ use Illuminate\Support\Facades\Http;
 
 
 Route::post('/subscribeToTopic', [FirebaseController::class, 'subscribeToTopic']);
-Route::get('/', function () {
-    return redirect('/login/admin');
-})->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('lang/{locale}', 'HomeController@lang')->name('lang');
 Route::get('terms-and-conditions', 'HomeController@terms_and_conditions')->name('terms-and-conditions');
 Route::get('about-us', 'HomeController@about_us')->name('about-us');
